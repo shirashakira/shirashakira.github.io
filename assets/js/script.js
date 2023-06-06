2342f39ff2e0d4f213f02c1eed6ef1da801d16fe
Nathan Levine <nathanlevine@Nathans-MacBook-Pro.local>
2023-06-06 17:50:43 -0400

    added all the html work

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,7 +103,7 @@ $(function() {
 
   //print button starts disabled
   const printButton = document.getElementById('makePrintButton');
-  //printButton.disabled = true;
+  printButton.disabled = true;
 
   //then we add checks for each input
   const turfName = document.getElementById('turfName');
@@ -112,120 +112,136 @@ $(function() {
   const rollEnd = document.getElementById('rollEnd');
   const containterNum = document.getElementById('containterNum');
   const rollFeet = document.getElementById('rollFeet');
+  const soNum = document.getElementById('soNum');
+  const turfTypesInput = document.getElementById('turfTypes')
 
   var isValidTurfName = false;
   var isValidDyeLot = false;
   var isValidRollStart = false;
   var isValidRollEnd = false;
+  var isValidSoNum = false;
   var isValidContainerNum = false;
   var isValidRollFeet = false;
+
+  turfTypes.addEventListener('change', function () {
+    const turfTypeSelected = turfTypesInput.value
+    if (turfTypeSelected === "Evernatural Premium") {
+      turfName.value = this.value.uppercase;
+      rollFeet.value = 100
+    } else if (turfTypeSelected === "Evernatural Classic") {
+      turfName.value = this.value.toUpperCase();
+      rollFeet.value = 100
+    } else if (turfTypeSelected === "Sequoia") {
+      turfName.value = this.value.toUpperCase();
+      rollFeet.value = 75
+    } else if (turfTypeSelected === "Sequoia Lite") {
+      turfName.value = this.value.toUpperCase();
+      rollFeet.value = 100
+    } else if (turfTypeSelected === "Coastal Plush") {
+      turfName.value = this.value.toUpperCase();
+      rollFeet.value = 100
+    } else if (turfTypeSelected === "Monte Verde") {
+      turfName.value = this.value.toUpperCase();
+      rollFeet.value = 75
+    } else if (turfTypeSelected === "Monte Verde Lite") {
+      turfName.value = this.value.toUpperCase();
+      rollFeet.value = 100
+    }  else if (turfTypeSelected === "Willow") {
+      turfName.value = this.value.toUpperCase();
+      rollFeet.value = 100
+    } else if (turfTypeSelected === "Professional Play") {
+      turfName.value = this.value.toUpperCase();
+      rollFeet.value = 100
+    } else if (turfTypeSelected === "Performance Putt") {
+      turfName.value = this.value.toUpperCase();
+      rollFeet.value = 100
+    }
+
+  });
+
+  // turfTypes.addEventListener('change', function(event)) {
+  //   console.log("does this hit? it will?");
+  //
+  //   const turfTypeSelected = turfTypesInput.value
+    // if (turfTypeSelected === "Evernatural Premium") {
+    //   rollFeet.value = "test"
+    // }
+  // });
 
   turfName.addEventListener('keyup', function (event) {
     isValidTurfName = turfName.checkValidity();
     console.log("test")
-    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidContainerNum && isValidRollFeet) {
+    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidSoNum && isValidContainerNum && isValidRollFeet) {
       printButton.disabled = false;
     } else {
-      //printButton.disabled = true;
+      printButton.disabled = true;
     }
   });
 
   dyeLot.addEventListener('keyup', function (event) {
     isValidDyeLot = dyeLot.checkValidity();
 
-    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidContainerNum && isValidRollFeet) {
+    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidSoNum && isValidContainerNum && isValidRollFeet) {
       printButton.disabled = false;
     } else {
-      //printButton.disabled = true;
+      printButton.disabled = true;
     }
   });
 
   rollStart.addEventListener('keyup', function (event) {
     isValidRollStart = rollStart.checkValidity();
 
-    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidContainerNum && isValidRollFeet) {
+    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidSoNum && isValidContainerNum && isValidRollFeet) {
       printButton.disabled = false;
     } else {
-      //printButton.disabled = true;
+      printButton.disabled = true;
     }
   });
 
   rollEnd.addEventListener('keyup', function (event) {
     isValidRollEnd = rollEnd.checkValidity();
 
-    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidContainerNum && isValidRollFeet) {
+    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidSoNum && isValidContainerNum && isValidRollFeet) {
       printButton.disabled = false;
     } else {
-      //printButton.disabled = true;
+      printButton.disabled = true;
+    }
+  });
+
+  soNum.addEventListener('keyup', function (event) {
+    isValidSoNum = soNum.checkValidity();
+
+    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidSoNum && isValidContainerNum && isValidRollFeet) {
+      printButton.disabled = false;
+    } else {
+      printButton.disabled = true;
     }
   });
 
   containterNum.addEventListener('keyup', function (event) {
     isValidContainerNum = containterNum.checkValidity();
 
-    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidContainerNum && isValidRollFeet) {
+    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidSoNum && isValidContainerNum && isValidRollFeet) {
       printButton.disabled = false;
     } else {
-      //printButton.disabled = true;
+      printButton.disabled = true;
     }
   });
 
   rollFeet.addEventListener('keyup', function (event) {
     isValidRollFeet = rollFeet.checkValidity();
 
-    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidContainerNum && isValidRollFeet) {
+    if (isValidTurfName && isValidDyeLot && isValidRollStart && isValidRollEnd && isValidSoNum && isValidContainerNum && isValidRollFeet) {
       printButton.disabled = false;
     } else {
-    //  printButton.disabled = true;
+      printButton.disabled = true;
     }
   });
 
 });
 
-function makeAndSaveTurfLabel() {
-  const htmlContent = '<html><body><h1>Hello, World!</h1></body></html>';
-  const fileName = 'example.html';
-
-  generateAndSaveHTMLToMachine(htmlContent, fileName);
-}
-
 function makeAndPrintTurfLabelPDF() {
-  //Max write your function to take the form data and turn that into PDFs that look like the example given to us.
-  //Save it to the turfdocs folder and then we can print from there using the last line on this function
-
-  //grab the form data
-  var name = document.querySelector('#turfName').value;
-  console.log("name: " + name)
-
-  var dyeLot = document.querySelector('#dyeLot').value;
-  console.log("dyeLot: " + dyeLot)
-
-  var rollStart = document.querySelector('#rollStart').value;
-  console.log("rollStart: " + rollStart)
-
-  var rollEnd = document.querySelector('#rollEnd').value;
-  console.log("rollEnd: " + rollEnd)
-
-  var containerNumber = document.querySelector('#containterNum').value;
-  console.log("containerNumber: " + containerNumber)
-
-  var rollFeet = document.querySelector('#rollFeet').value;
-  console.log("rollFeet: " + rollFeet)
-
-  /*
-  //for roll in rollStart...rollEnd {
-    //make the PDF
-  }
-  */
-  //printJS('../turfdocs/example.pdf')
-  const htmlContent = `
-          <html>
-            <body>
-              <h1>Turf Name: ${name}</h1>
-              <h2>Roll Start: ${rollStart}</h2>
-            </body>
-          </html>
-        `;
+  const htmlContent = generateHTMLOutputForTurfLabel()
 
   const options = {
     jsPDF: { format: 'a4', orientation: 'landscape' }
@@ -235,8 +251,6 @@ function makeAndPrintTurfLabelPDF() {
     const pdfUrl = URL.createObjectURL(pdfBlob);
     printJS(pdfUrl);
   });
-
-  //generateAndSaveHTMLToFileStructure()
 }
 
 function leadingZeros(input) {
@@ -245,33 +259,209 @@ function leadingZeros(input) {
   }
 }
 
-//this is if we want to save it to the machine
-function generateAndSaveHTMLToMachine(htmlContent, fileName) {
-  // Create a blob object with the HTML content
-  const blob = new Blob([htmlContent], { type: 'text/html' });
+function generateHTMLOutputForTurfLabel() {
+  //grab the form data
+  var rollStart = document.querySelector('#rollStart').value;
+  var rollEnd = document.querySelector('#rollEnd').value;
 
-  // Create a URL for the blob
-  const url = URL.createObjectURL(blob);
+  var mergedHTML = setupHTMLBeginning()
+  for (let i = rollStart; i <= rollEnd; i++) {
+    if (i > 0) {
+      let pageBreak = '<div style="page-break-before: always;"> </div>'
+      if (i < 2) {
+        mergedHTML = mergedHTML + setupTurfLabel(i)
+      } else {
+        mergedHTML = mergedHTML + pageBreak + setupTurfLabel(i)
+      }
+    }
+  }
 
-  // Create a link element
-  const link = document.createElement('a');
-  link.href = url;
-  link.download = fileName;
+  mergedHTML = mergedHTML + setupHTMLEnding()
 
-  // Simulate a click on the link to trigger the file download
-  link.click();
-
-  // Clean up the URL object
-  URL.revokeObjectURL(url);
+  return mergedHTML;
 }
 
-//this is if we want to save it to the file structure
-function generateAndSaveHTMLToFileStructure(htmlContent, fileName) {
-  fs.writeFile(fileName, htmlContent, (err) => {
-    if (err) {
-      console.error('Error saving HTML file:', err);
-      return;
-    }
-    console.log('HTML file saved successfully!');
-  });
+function setupHTMLBeginning() {
+  const html = `
+  <!DOCTYPE html>
+  <html">
+    <head>
+      <style>
+          #turfWrapper {
+              width:100%;
+              overflow: hidden; /* will contain if #first is longer than #second */
+          }
+          #turfFirst {
+              width: 49%;
+              float:left; /* add this */
+          }
+          #turfSecond {
+              width: 49%;
+              overflow: hidden; /* if you don't want #second to wrap below #first */
+          }
+          #turfTtitle {
+              font-family: Arial, Helvetica, sans-serif;
+              font-size: 40px;
+          }
+
+          table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+          }
+          th, td {
+            padding: 5px;
+            padding-bottom: 20px;
+            text-align: left;
+            color: black;
+          }
+
+      </style>
+    </head>
+    <body>
+  `
+  return html
+}
+
+function setupHTMLEnding() {
+  const html = `
+        </body>
+      </html>`
+  return html
+}
+
+function setupTurfLabel(rollNumber) {
+  var name = document.querySelector('#turfName').value;
+  var dyeLot = document.querySelector('#dyeLot').value;
+  var containerNumber = document.querySelector('#containterNum').value;
+  var rollFeet = document.querySelector('#rollFeet').value;
+  var soNum = document.querySelector('#soNum').value;
+
+  const html = `
+      <div id="turfWrapper" style="margin-top: 40px; margin-left: 20px;">
+          <div id="turfFirst" style="">
+              <div style="color: black; font-size: 20px; font-family: Calibri, Helvetica, sans-serif;"><b>NAME</b></div>
+              <div style="color: black; font-size: calc(2vw + 15px); font-family: Calibri, Helvetica, sans-serif; font-weight: bold;">${name}</div>
+              <br>
+              <div style="color: black; font-size: 20px; font-family: Calibri, Helvetica, sans-serif;"><b>DYE LOT</b></div>
+              <div style="color: black; font-size: 7.5vw; font-family: Calibri, Helvetica, sans-serif; font-weight: bold;">${dyeLot}</div>
+              <br>
+              <div style="color: black; font-size: 20px; font-family: Calibri, Helvetica, sans-serif;"><b>ROLL #</b></div>
+              <div style="margin-top:-30px; text-align: center; color: black; font-size: 180px; font-family: Calibri, Helvetica, sans-serif; font-weight: bold;">${rollNumber}</div>
+          </div>
+          <div id="turfSecond">
+            <div style="color: black; font-size: 20px; font-family: Calibri, Helvetica, sans-serif;"><b>NAME</b></div>
+            <div style="color: black; font-size: calc(2vw + 15px); font-family: Calibri, Helvetica, sans-serif; font-weight: bold;">${name}</div>
+            <br>
+            <div style="color: black; font-size: 20px; font-family: Calibri, Helvetica, sans-serif;"><b>DYE LOT</b></div>
+            <div style="color: black; font-size: 7.5vw; font-family: Calibri, Helvetica, sans-serif; font-weight: bold;">${dyeLot}</div>
+            <br>
+            <div style="color: black; font-size: 20px; font-family: Calibri, Helvetica, sans-serif;"><b>ROLL #</b></div>
+            <div style="margin-top: -30px; text-align: center; color: black; font-size: 180px; font-family: Calibri, Helvetica, sans-serif; font-weight: bold;">${rollNumber}</div>
+          </div>
+      </div>
+
+      <div style="page-break-before: always;"> </div>
+
+      <div style="margin-top: 20px; margin-left: 20px;">
+
+          <table style="width:35%">
+            <tr>
+              <th>TYPE:</th>
+              <td>${name}</td>
+            </tr>
+            <tr>
+              <th>SO #:</th>
+              <td>${soNum}</td>
+            </tr>
+            <tr>
+              <th>CONTAINER #:</th>
+              <td>${containerNumber}</td>
+            </tr>
+          </table>
+
+          <div style="color:black; margin-top: 20px; border: 3px solid black; max-width: 25%; text-align: center; padding-bottom: 10px; padding-top: 10px;">STARTED WITH <b>${rollFeet}</b> LINEAR FEET</div>
+
+          <table style="margin-top: 20px;">
+              <thead>
+              <tr>
+                  <th></th>
+                  <th style="padding-left:60px;padding-right:60px;">&nbsp;&nbspDATE &nbsp;&nbsp;</th>
+                  <th style="padding-left:50px;padding-right:50px;">&nbsp;&nbsp;INVOICE # / TRANSFER #&nbsp;&nbsp;</th>
+                  <th>&nbsp;&nbsp;TOTAL LINEAR FEET CUT&nbsp;&nbsp;</th>
+                  <th>&nbsp;&nbsp;TOTAL LINEAR FEET LEFT&nbsp;&nbsp;</th>
+                  <th>&nbsp;&nbsp;CUTTER (INITIALS)&nbsp;&nbsp;</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                  <td>1</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+              </tr>
+              <tr>
+                  <td>2</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+              </tr>
+              <tr>
+                  <td>3</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+              </tr>
+              <tr>
+                  <td>4</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+              </tr>
+              <tr>
+                  <td>5</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+              </tr>
+              <tr>
+                  <td>6</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+              </tr>
+              <tr>
+                  <td>7</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+              </tr>
+              <tr>
+                  <td>8</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+              </tr>
+              <tbody>
+          </table>
+
+      </div>
+  `;
+
+  return html
 }
