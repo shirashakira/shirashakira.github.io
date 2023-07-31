25a5642044ab1ac3a520d1dd6716c78f230886a1
Nathan Levine <nathanlevine@Nathans-MacBook-Pro.local>
2023-07-30 20:24:00 -0400

    parsing the data for the uploaded excel spreadsheet

--- assets/js/storeLabel.js
+++ assets/js/storeLabel.js
@@ -3,11 +3,46 @@
 $(function() {
 
 
-  const excelInput = document.getElementById('customFileUpload')
+  const excelInput = document.getElementById('customFileUpload');
   excelInput.addEventListener('change', function () {
     document.querySelector("#fileUploadLabel").textContent = this.files[0].name;
 
+    const file = this.files[0]; // Get the selected file (the first file in the list)
+
+    if (file) {
+      // Create a new FileReader instance
+      const reader = new FileReader();
+
+      // Define a function to handle the file reading completion
+      reader.onload = function (e) {
+        const arrayBuffer = e.target.result;
+
+        // Parse the Excel data using SheetJS
+        const workbook = XLSX.read(arrayBuffer, { type: 'array' });
+
+        // Assuming the first sheet is the one containing the data
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+
+        // Convert the worksheet to an array of objects
+        const parsedData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+        //console.log('Parsed data:', parsedData);
+
+        // You can now access the data as an array of objects
+        // Each row in the spreadsheet will be an object in the array
+        // For example, to access the first row's "Product" value:
+        //shows up like:
+        //['Ln#', 'Product', 'Description', 'Qty Ord']
+        //[1, 401005, '1/2" SCH 40 PVC TEE FITTING SXSXS FOR IRRIGATION', 25]
+        console.log('Product of the first row:', parsedData[1][1]);
+      };
+
+      // Read the file as an ArrayBuffer
+      reader.readAsArrayBuffer(file);
+    }
   });
+
+
 
 });
 
